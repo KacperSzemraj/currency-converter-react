92a3ca9f442e9f241c977b82e2c87e3fbc8342f1
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-08-04 10:57:58 +0200

    calculate result interaction added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "./Header";
 import Container from "./Container";
 import Form from "./Container/Form";
@@ -8,13 +8,32 @@ import Footer from "./Footer";
 import { currencies } from './currencies';
 
 function App() {
+
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+      .find(({ short }) => short === currency)
+      .rate;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
+
   return (
     <>
       <Header title="Kalkulator Walutowy" />
       <Container>
-        <Form />
+        <Form
+          calculateResult={calculateResult}
+        />
         <Paragraph />
-        <Result />
+        <Result
+          result={result}
+        />
       </Container>
       <Footer />
     </>
