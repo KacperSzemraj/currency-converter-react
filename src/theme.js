f4bf19eed7f896d97ed71da45ddf6651c9ab3ab9
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-10 11:28:41 +0200

    theme.js created, ThemeProvider added

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    color: {
+        mineShaft: "#222222",
+        white: "#fff",
+        green: "#008000",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
