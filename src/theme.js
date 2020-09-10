2ce863a5980e87cc0ec276d6528c7a150026675b
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-10 11:59:45 +0200

    colors added to the theme

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,8 @@ export const theme = {
     color: {
         mineShaft: "#222222",
         white: "#fff",
+        pippin: "#ffe5e5",
+        snowyMint: "#ccffcc",
         green: "#008000",
     },
     breakpoint: {
