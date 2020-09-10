33eaf0974eaef4c28779e713ab6100914eb5bf79
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-10 12:06:41 +0200

    colors added to the theme

--- src/Container/Result/styled.js
+++ src/Container/Result/styled.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 export const TextBox = styled.div`
     padding: 10px;
-    background-color: #fff;
-    border: 1px solid #222222;
+    background-color: ${({ theme }) => theme.color.white};
+    border: 1px solid ${({ theme }) => theme.color.mineShaft};
     border-radius: 4px;
-    box-shadow: 1px 1px #222222;
+    box-shadow: 1px 1px ${({ theme }) => theme.color.mineShaft};
     text-align: center;
 `;
 
@@ -18,14 +18,14 @@ export const ResultText = styled.span`
 `;
 
 export const SourceAmount = styled.span`
-    color: hsl(120, 100%, 25%);
+    color: ${({ theme }) => theme.color.green};
     font-weight: 600;
     font-size: 24px;
     letter-spacing: 1px;
 `;
 
 export const TargetAmount = styled.span`
-    color: green;
+    color: ${({ theme }) => theme.color.green};
     font-weight: 800;
     font-size: 24px;
     letter-spacing: 1px;
