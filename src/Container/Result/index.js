88ac36f5a2f1b21beae87d2f60fa28956416b617
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-10 13:11:08 +0200

    TextBox will show up after the calculation

--- src/Container/Result/index.js
+++ src/Container/Result/index.js
@@ -2,21 +2,19 @@ import React from "react";
 import { TextBox, ResultText, SourceAmount, TargetAmount } from "./styled";
 
 const Result = ({ result }) => (
-    <TextBox>
-        <ResultText>
-            {result !== undefined && (
-                <>
-                    <SourceAmount>
-                        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;
+    result !== undefined && (
+        <TextBox>
+            <ResultText>
+                <SourceAmount>
+                    {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;
                     </SourceAmount>
                     to w przeliczeniu&nbsp;
                     <TargetAmount>
-                        {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
-                    </TargetAmount>
-                </>
-            )}
-        </ResultText>
-    </TextBox>
+                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+                </TargetAmount>
+            </ResultText>
+        </TextBox>
+    )
 );
 
 export default Result;
