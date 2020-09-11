225f56fe9322e7f0038ea358cbf81fc0fd01b407
Kacper Szemraj <kacperprzemyslawszemraj@gmail.com>
2020-09-11 08:55:35 +0200

    background changed

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import background from "./converter-background.jpg";
+import background from "./background.jpg";
 
 export const GlobalStyle = createGlobalStyle`
     html {
